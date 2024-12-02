96f281245ab7962419b3a907459cd173427289b2
anya-gray <67169358+anya-gray@users.noreply.github.com>
2024-12-02 12:09:17 +0000

    T, flux development; diffusion ChatGPT suggestion

--- transect2D.m
+++ transect2D.m
@@ -28,8 +28,7 @@ iz = [    1:Nz   ];          % isothermal start boundary, Nz+1 will be changed b
 
 % set initial condition for temperature at cell centres
 
-Tx = Ttop + (Tbot-Ttop) ./ Nx .* Xc;
-Tz   = Ttop + (Tbot-Ttop) ./ Nz .* Zc;  % initialise T array on linear gradient
+T = T0 + dTdz_boundaries(2)*Zc;  % initialise T array (based on what emily wrote)
 
 Tin = T;                                         % store initial condition
 Ta  = T;                                         % initialise analytical solution
@@ -46,12 +45,13 @@ while t <= tend
     t = t+dt;
     tau = tau+1;
     
+    R1 = diffusion2D(T, k0, h, dTdz_boundaries(2));
     % 4th-order Runge-Kutta time integration scheme
     % NEED TO MAKE THIS 2D !!!
-    R1 = diffusion(Tx, Tz, k0, h, ix, iz);
-    R2 = diffusion(T + R1*dt/2, k0, h, ix, iz);
-    R3 = diffusion(T + R2*dt/2, k0, h, ix, iz);
-    R4 = diffusion(T + R3*dt, k0, h, ix, iz);
+    % R1 = diffusion(dTdz_boundaries(1), dTdz_boundaries(2), k0);
+    % R2 = diffusion(T + R1*dt/2, k0, h, ix, iz);
+    % R3 = diffusion(T + R2*dt/2, k0, h, ix, iz);
+    % R4 = diffusion(T + R3*dt, k0, h, ix, iz);
     
     % calculate 
     source = Hr ./ rho ./ Cp;
@@ -72,19 +72,68 @@ end
 
 
 % Function to calculate diffusion rate
-function dTdt = diffusion(f, k0, h, ix, iz)
+% Dtdzt, dTdzb = top and bottom heat flux
+% k0 diffusivity
+function dTdt = diffusion(dTdzt, dTdzb, k0)
 
 % calculate heat flux by diffusion
-qx = - diff( k0(:, ix) ) .* diff( f(:, ix) )/h;
-qz = - diff( k0(iz, :) ) .* diff( f(iz, :) )/h;
+% qz = - diff( k0(iz, :) ) .* diff( f(iz, :) )/h;
+% qx = - diff( k0(:, ix) ) .* diff( f(:, ix) )/h;
+
+% use dTdz to 
+qz(1,:) = k0(1,:)*dTdzt;
+qz(end,:) = k0(1,:)*dTdzb;
+
+size(qz(1,:))
+size(qz(end,:))
+size(qz)
+
+% qx(end,:) = k0(end,:)*
 
 % disp(size(qx));
 
 % calculate flux balance for rate of change (2nd derivative)
-dTdt = - diff(qx + qz)/h;
+% dTdt = - diff(qx + qz)/h;
 
 end
 
+
+% SUGGESTION FROM CHATGPT
+function dTdt = diffusion2D(f, k0, h, dTdzb)
+
+% Get the size of the temperature grid
+[nz, nx] = size(f);
+
+% Preallocate arrays for fluxes
+qx = zeros(nz, nx-1); % Flux in the x-direction
+qz = zeros(nz-1, nx); % Flux in the z-direction
+
+% Calculate heat flux in the x-direction (horizontal)
+for j = 1:nz
+    qx(j, :) = -k0(j, 1:end-1) .* diff(f(j, :)) / h;
+end
+
+% Calculate heat flux in the z-direction (vertical)
+for i = 1:nx
+    qz(:, i) = -k0(1:end-1, i) .* diff(f(:, i)) / h;
+end
+
+% Apply boundary condition at the bottom (z = depth)
+% qz(end, :) corresponds to the flux at the bottom
+qz(end+1, :) = k0(end, :) * dTdzb;
+
+% Apply zero-flux boundary condition at the top (z = 0)
+% No heat flux means qz(1, :) is effectively 0
+qz = [zeros(1, nx); qz];
+
+% Calculate flux divergence
+dqx_dx = [qx(:, 1), diff(qx, 1, 2), -qx(:, end)] / h; % Flux change in x-direction
+dqz_dz = diff(qz, 1, 1) / h; % Flux change in z-direction
+
+% Rate of temperature change
+dTdt = dqx_dx + dqz_dz;
+
+end
 
 
 % Function to make output figure
